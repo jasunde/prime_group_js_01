1b5a92c85210c79882e8ced3f1c3408dd4ec55bd
jasunde <jlaurits@gmail.com>
2016-10-27 13:01:36 -0500

    Basic bonus percentage calculator added

--- group_01.js
+++ group_01.js
@@ -6,3 +6,46 @@ var robert = ["Robert", "26835", "66000", 1];
 var mayella = ["Mayella", "89068", "35000", 2];
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
+
+for (var i = 0; i < employees.length; i++) {
+  var employee = employees[i];
+  console.log(bonusArrayBuilder(employee));
+}
+
+// Builds an array with [name, bonus percentage, total compensation, rounded total compensation]
+function bonusArrayBuilder(employee) {
+  [name, id, salary, rating] = employee;
+
+  var bonusPercentage = bonusCalculator(id, salary, rating);
+  return [name, bonusPercentage];
+}
+
+function bonusCalculator(id, salary, rating) {
+  var bonusPercentage = 0;
+
+  switch(rating) {
+    case 3:
+      bonusPercentage = 4;
+      break;
+    case 4:
+      bonusPercentage = 6;
+      break;
+    case 5:
+      bonusPercentage = 10;
+      break;
+  }
+
+  if(id.length == 4) {
+    bonusPercentage += 5;
+  }
+
+  if(bonusPercentage > 13) {
+    bonusPercentage = 13;
+  }
+
+  if(bonusPercentage < 0) {
+    bonusPercentage = 0;
+  }
+
+  return bonusPercentage / 100;
+}
